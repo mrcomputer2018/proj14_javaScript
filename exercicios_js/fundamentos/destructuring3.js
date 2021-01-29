8ea18d6c378a1df84a78bfc86e1d789ccfca80b2
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-01-29 16:02:56 -0300

    destructuring com funcao e objeto

--- exercicios_js/fundamentos/destructuring3.js
+++ exercicios_js/fundamentos/destructuring3.js
@@ -1,1 +1,10 @@
-// Operador que tira da estrutura uma coisa // Estrutura seria um Array
+// Operador que tira da estrutura uma coisa // Estrutura seria uma Funcao com objeto
+function rand({min = 0, max = 1000}) {
+    const valor = Math.random() * (max-min)+ min
+    return Math.floor(valor) // Floor - Retorna o maior inteiro menor ou igual ao seu numérico
+}
+
+const obj = {min: 50, max: 60}
+console.log(rand(obj)) // Resultado sera randomico entre 50 e 60
+console.log(rand({min:800})) // Resultado sera randomico entre 800 e 1000
+console.log(rand({}))// Resultado sera randomico entre 0 e 1000
