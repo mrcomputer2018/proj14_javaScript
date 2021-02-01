9e01a5301655cf91c9ddd4bfb0612bb093820ceb
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-02-01 16:03:19 -0300

    Usando variaveis

--- exercicios_js/livro_casa/testando_idades.js
+++ exercicios_js/livro_casa/testando_idades.js
@@ -1,3 +1,4 @@
 document.write('Minha idade é :' + 25)
 document.write('<br>A somade nossas idades é:' + (25 + 32 + 45))
 document.write('<br> A Media de nossas idades é:' + ((25 + 32 + 45 ) /3))
+document.write('<br> O numero de gerações ate hoje é:' + (2021 / 28))
