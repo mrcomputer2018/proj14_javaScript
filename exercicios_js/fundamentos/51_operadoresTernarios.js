d95e5dd08f8f2a132d10acef63cb8ba8900e148e
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-05-18 15:18:49 -0300

    operador ternario

---
+++ exercicios_js/fundamentos/51_operadoresTernarios.js
@@ -0,0 +1,7 @@
+// Funcao arrow
+const resultado = nota => nota >= 7 ? "Aprovado" : "Reprovado"
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+const impressao = nota >= 7 ? "Aprovado" : "Reprovado"
