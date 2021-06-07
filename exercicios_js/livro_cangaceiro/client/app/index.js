ebe0c03e10cea31903f67da9cef76f84d0d288f2
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-07 15:21:45 -0300

    sletor de campos

---
+++ exercicios_js/livro_cangaceiro/client/app/index.js
@@ -0,0 +1,8 @@
+//* Buscando os elementos atraves do seletor CSS
+var campos = [
+    document.querySelector("#data"),
+    document.querySelector("#valor"),
+    doocument.querySelector("#quantidade")
+];
+
+console.log(campos);
