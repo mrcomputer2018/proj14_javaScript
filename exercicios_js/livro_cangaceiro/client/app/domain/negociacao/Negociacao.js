36a6617b61bb730446ca9292b960dd172baa063c
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-08 15:21:47 -0300

    criando getters

--- exercicios_js/livro_cangaceiro/client/app/domain/negociacao/Negociacao.js
+++ exercicios_js/livro_cangaceiro/client/app/domain/negociacao/Negociacao.js
@@ -4,13 +4,26 @@ class Negociacao {
     //* Construtor
     constructor(data, quantidade, valor) {
         //* data atual
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     //* Metodo
     obtemVolume() {
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
+    }
+
+    //*Getters
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
     }
 }
