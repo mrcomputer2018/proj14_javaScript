4329c4dfead30c344de34e8fada6a3fe3348cc78
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-14 16:57:45 -0300

    app.js v2.0

--- exercicios_js/livro_cangaceiro/client/app/app.js
+++ exercicios_js/livro_cangaceiro/client/app/app.js
@@ -1,6 +1,4 @@
 let controller = new NegociacaoController();
 
 //* Associa o evento de submissao do formulario a chamada do metodo ADICIONA em controller
-document.querySelector('.form').addEventListener('submit', function(event) {
-    controller.adiciona(event);
-})
+document.querySelector('.form').addEventListener('submit', controller.adiciona);
