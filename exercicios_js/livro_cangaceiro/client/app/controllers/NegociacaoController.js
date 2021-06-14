a94ddbbbc780e6ee332316a5020b359a5028a054
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-14 17:09:22 -0300

    NeegociacaoController v2.0

--- exercicios_js/livro_cangaceiro/client/app/controllers/NegociacaoController.js
+++ exercicios_js/livro_cangaceiro/client/app/controllers/NegociacaoController.js
@@ -4,6 +4,13 @@ class NegociacaoController {
         //*cancelando submissao de formulario
         event.preventFefault();
 
-        alert("Chamei acao no controller!!!");
+        let inputData = document.querySelector('#data');
+        let inputQuantidade = document.querySelector('#quantidade');
+        let inputValor = document.querySelector('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+
     }
 }
