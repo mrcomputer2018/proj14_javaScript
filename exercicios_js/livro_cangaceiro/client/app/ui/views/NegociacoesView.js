68d2b157b1663cf65ba004a1ad14515efe4450dd
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-16 11:59:24 -0300

    criando template de table

--- exercicios_js/livro_cangaceiro/client/app/ui/views/NegociacoesView.js
+++ exercicios_js/livro_cangaceiro/client/app/ui/views/NegociacoesView.js
@@ -2,6 +2,24 @@ class NegociacoesView {
 
     //* Metodo
     template() {
-        
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>data</th>
+                        <th>quantidade</th>
+                        <th>valor</th>
+                        <th>volume</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                </tbody>
+
+                <tfoot>
+
+                </tfoot>
+            </table>`
     }
 }
